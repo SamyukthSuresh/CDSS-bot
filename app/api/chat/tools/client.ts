0bcb962aceed66b869ed02721dbe5b8e95fa9f59
Samyukth S S <48058026+SamyukthSuresh@users.noreply.github.com>
2025-11-26 23:20:57 +0530

    Update client.ts

--- app/api/chat/tools/client.ts
+++ app/api/chat/tools/client.ts
@@ -1,9 +1,9 @@
 import { Pinecone } from "@pinecone-database/pinecone";
 import OpenAI from "openai";
 
+
 export const pinecone = new Pinecone({
   apiKey: process.env.PINECONE_API_KEY!,
-  environment: process.env.PINECONE_ENV, // or region depending on config
 });
 
 export const openai = new OpenAI({
